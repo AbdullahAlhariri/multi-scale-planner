c0949f8ea4e359ba73853d04b873c6503f2da407
Abdullah Alhariri <alhariri.abdu.2018@gmail.com>
2023-12-07 16:38:56 +0100

    Adding prisma cli client and migrations

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          {/*<DeployButton />*/}
           <div></div>
           <AuthButton />
         </div>
@@ -13,14 +12,9 @@ export default async function Index() {
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          test
+          <button type={'button'}>Create goal +</button>
         </main>
       </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        test
-      </footer>
     </div>
   )
 }
