046771d2dd3df43da289c9a775ae51a78fe5d5e7
Abdullah Alhariri <alhariri.abdu.2018@gmail.com>
2023-12-07 16:14:27 +0100

    removing canInitSupabaseClient

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
 import AuthButton from '../components/AuthButton'
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
-
 export default async function Index() {
-  const cookieStore = cookies()
-
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
-
-  const isSupabaseConnected = canInitSupabaseClient()
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
